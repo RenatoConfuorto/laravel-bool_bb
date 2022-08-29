8f99fb539a565b4470e0a02ec071175d82c6e9e0
SydVic <fabrizio.bocca27@gmail.com>
2022-08-29 16:09:21 +0200

    aggiunti controlli front-end su form create/update

--- resources/views/user/apartment/edit.blade.php
+++ resources/views/user/apartment/edit.blade.php
@@ -21,14 +21,14 @@
     {{-- TITLE --}}
     <div class="mb-3">
       <label for="title" class="form-label">Titolo</label>
-      <input type="text" class="form-control" id="title" name="title" value="{{ old('title') ? old('title') : $apartment->title }}">
+      <input type="text" class="form-control" id="title" name="title" value="{{ old('title') ? old('title') : $apartment->title }}" required minlength="4" maxlength="255">
     </div>
     {{-- /TITLE --}}
 
     {{-- PRICE --}}
     <div class="mb-3">
       <label for="price" class="form-label">Prezzo</label>
-      <input type="number" class="form-control" id="price" name="price" value="{{ old('price') ? old('price') : $apartment->price }}">
+      <input type="number" class="form-control" id="price" name="price" value="{{ old('price') ? old('price') : $apartment->price }}" required min="0" max="9999">
     </div>
     {{-- /PRICE --}}
 
@@ -36,49 +36,49 @@
     <div class="mb-3">
       <label for="description" class="form-label">Descrizione</label>
       {{-- TEXT area non ha attributo value quindi old() va all'interno del tag --}}
-      <textarea type="text" class="form-control" id="description" name="description" rows="5">{{ old('description') ? old('description') : $apartment->description }}</textarea>
+      <textarea type="text" class="form-control" id="description" name="description" rows="5" required minlength="10" maxlength="10000">{{ old('description') ? old('description') : $apartment->description }}</textarea>
     </div>
     {{-- /DESCRIPTION --}}
 
     {{-- ROOMS NUMBER --}}
     <div class="mb-3">
       <label for="rooms_number" class="form-label">Numero di stanze</label>
-      <input type="number" class="form-control" id="rooms_number" name="rooms_number" value="{{ old('rooms_number') ? old('rooms_number') : $apartment->rooms_number }}">
+      <input type="number" class="form-control" id="rooms_number" name="rooms_number" value="{{ old('rooms_number') ? old('rooms_number') : $apartment->rooms_number }}" required min="1" max="255">
     </div>
     {{-- /ROOMS NUMBER --}}
 
     {{-- BATHROOMS NUMBER --}}
     <div class="mb-3">
       <label for="bathrooms_number" class="form-label">Numero di bagni</label>
-      <input type="number" class="form-control" id="bathrooms_number" name="bathrooms_number" value="{{ old('bathrooms_number') ? old('bathrooms_number') : $apartment->bathrooms_number }}">
+      <input type="number" class="form-control" id="bathrooms_number" name="bathrooms_number" value="{{ old('bathrooms_number') ? old('bathrooms_number') : $apartment->bathrooms_number }}" required min="1" max="255">
     </div>
     {{-- /BATHROOMS NUMBER --}}
 
     {{-- BEDS NUMBER --}}
     <div class="mb-3">
       <label for="beds_number" class="form-label">Numero di letti</label>
-      <input type="number" class="form-control" id="beds_number" name="beds_number" value="{{ old('beds_number') ? old('beds_number') : $apartment->beds_number }}">
+      <input type="number" class="form-control" id="beds_number" name="beds_number" value="{{ old('beds_number') ? old('beds_number') : $apartment->beds_number }}" required min="1" max="255">
     </div>
     {{-- /BEDS NUMBER --}}
 
     {{-- MQS --}}
     <div class="mb-3">
       <label for="mqs" class="form-label">Metri quadrati</label>
-      <input type="number" class="form-control" id="mqs" name="mqs" value="{{ old('mqs') ? old('mqs') : $apartment->mqs }}">
+      <input type="number" class="form-control" id="mqs" name="mqs" value="{{ old('mqs') ? old('mqs') : $apartment->mqs }}" required min="1" max="65535">
     </div>
     {{-- /MQS --}}
 
     {{-- ADDRESS --}}
     <div class="mb-3">
       <label for="address" class="form-label">Indirizzo</label>
-      <input type="text" class="form-control" id="address" name="address" value="{{ old('address') ? old('address') : $apartment->address }}">
+      <input type="text" class="form-control" id="address" name="address" value="{{ old('address') ? old('address') : $apartment->address }}" required minlength="4" maxlength="255">
     </div>
     {{-- /ADDRESS --}}
 
     {{-- COVER IMAGE --}}
     <div class="mb-3">
       <label for="image">Aggiungi un immagine</label>
-      <input type="file" id="image" name="image">
+      <input type="file" id="image" name="image" accept="image/*">
     </div>
     {{-- /COVER IMAGE --}}
 
