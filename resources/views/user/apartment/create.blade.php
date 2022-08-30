5980496cb4373e4034fc6035bd3defba393e4ce6
SydVic <fabrizio.bocca27@gmail.com>
2022-08-30 12:02:11 +0200

    aggiunti asterischi pe campi obligatori create/edit e chechek input in edit

--- resources/views/user/apartment/create.blade.php
+++ resources/views/user/apartment/create.blade.php
@@ -19,14 +19,14 @@
 
     {{-- TITLE --}}
     <div class="mb-3">
-      <label for="title" class="form-label">Titolo</label>
+      <label for="title" class="form-label">Titolo *</label>
       <input type="text" class="form-control" id="title" name="title" value="{{ old('title') }}" required minlength="4" maxlength="255">
     </div>
     {{-- /TITLE --}}
 
     {{-- PRICE --}}
     <div class="mb-3">
-      <label for="price" class="form-label">Prezzo</label>
+      <label for="price" class="form-label">Prezzo *</label>
       <input type="number" class="form-control" id="price" name="price" value="{{ old('price') }}" required min="0" max="9999.99" step="0.01">
     </div>
     {{-- /PRICE --}}
@@ -41,49 +41,49 @@
 
     {{-- ROOMS NUMBER --}}
     <div class="mb-3">
-      <label for="rooms_number" class="form-label">Numero di stanze</label>
+      <label for="rooms_number" class="form-label">Numero di stanze *</label>
       <input type="number" class="form-control" id="rooms_number" name="rooms_number" value="{{ old('rooms_number') }}" required min="1" max="255">
     </div>
     {{-- /ROOMS NUMBER --}}
 
     {{-- BATHROOMS NUMBER --}}
     <div class="mb-3">
-      <label for="bathrooms_number" class="form-label">Numero di bagni</label>
+      <label for="bathrooms_number" class="form-label">Numero di bagni *</label>
       <input type="number" class="form-control" id="bathrooms_number" name="bathrooms_number" value="{{ old('bathrooms_number') }}" required min="1" max="255">
     </div>
     {{-- /BATHROOMS NUMBER --}}
 
     {{-- BEDS NUMBER --}}
     <div class="mb-3">
-      <label for="beds_number" class="form-label">Numero di letti</label>
+      <label for="beds_number" class="form-label">Numero di letti *</label>
       <input type="number" class="form-control" id="beds_number" name="beds_number" value="{{ old('beds_number') }}" required min="1" max="255">
     </div>
     {{-- /BEDS NUMBER --}}
 
     {{-- MQS --}}
     <div class="mb-3">
-      <label for="mqs" class="form-label">Metri quadrati</label>
+      <label for="mqs" class="form-label">Metri quadrati *</label>
       <input type="number" class="form-control" id="mqs" name="mqs" value="{{ old('mqs') }}" required min="10" max="65535">
     </div>
     {{-- /MQS --}}
 
     {{-- ADDRESS --}}
     <div class="mb-3">
-      <label for="address" class="form-label">Indirizzo</label>
+      <label for="address" class="form-label">Indirizzo *</label>
       <input type="text" class="form-control" id="address" name="address" value="{{ old('address') }}" required minlength="4" maxlength="255">
     </div>
     {{-- /ADDRESS --}}
 
     {{-- COVER IMAGE --}}
     <div class="mb-3">
-      <label for="image-cover">Aggiungi un immagine</label>
+      <label for="image-cover">Aggiungi un immagine *</label>
       <input type="file" id="image-cover" name="image-cover" accept=".jpg, .jpeg, .png, .bmp, .gif, .svg, .webp">
     </div>
     {{-- /COVER IMAGE --}}
 
     {{-- EXTRA SERVICES --}}
     <div class="extra-service">
-    <p>Servizi Extra</p>
+    <p>Servizi Extra *</p>
       <ul>
         @foreach ($extra_services as $extra_service)
         <li>
@@ -103,7 +103,7 @@
     <div class="form-check">
       <input class="form-check-input" type="checkbox" value="" id="flexCheckChecked" checked>
       <label class="form-check-label" for="flexCheckChecked">
-        Vuoi rendere questo appartamento visibile?
+        Vuoi rendere questo appartamento visibile? *
       </label>
     </div>
     {{-- /VISIBILITY --}}
--- resources/views/user/apartment/edit.blade.php
+++ resources/views/user/apartment/edit.blade.php
@@ -20,14 +20,14 @@
 
     {{-- TITLE --}}
     <div class="mb-3">
-      <label for="title" class="form-label">Titolo</label>
+      <label for="title" class="form-label">Titolo *</label>
       <input type="text" class="form-control" id="title" name="title" value="{{ old('title') ? old('title') : $apartment->title }}" required minlength="4" maxlength="255">
     </div>
     {{-- /TITLE --}}
 
     {{-- PRICE --}}
     <div class="mb-3">
-      <label for="price" class="form-label">Prezzo</label>
+      <label for="price" class="form-label">Prezzo *</label>
       <input type="number" class="form-control" id="price" name="price" value="{{ old('price') ? old('price') : $apartment->price }}" required min="0" max="9999" step="0.01">
     </div>
     {{-- /PRICE --}}
@@ -42,48 +42,48 @@
 
     {{-- ROOMS NUMBER --}}
     <div class="mb-3">
-      <label for="rooms_number" class="form-label">Numero di stanze</label>
+      <label for="rooms_number" class="form-label">Numero di stanze *</label>
       <input type="number" class="form-control" id="rooms_number" name="rooms_number" value="{{ old('rooms_number') ? old('rooms_number') : $apartment->rooms_number }}" required min="1" max="255">
     </div>
     {{-- /ROOMS NUMBER --}}
 
     {{-- BATHROOMS NUMBER --}}
     <div class="mb-3">
-      <label for="bathrooms_number" class="form-label">Numero di bagni</label>
+      <label for="bathrooms_number" class="form-label">Numero di bagni *</label>
       <input type="number" class="form-control" id="bathrooms_number" name="bathrooms_number" value="{{ old('bathrooms_number') ? old('bathrooms_number') : $apartment->bathrooms_number }}" required min="1" max="255">
     </div>
     {{-- /BATHROOMS NUMBER --}}
 
     {{-- BEDS NUMBER --}}
     <div class="mb-3">
-      <label for="beds_number" class="form-label">Numero di letti</label>
+      <label for="beds_number" class="form-label">Numero di letti *</label>
       <input type="number" class="form-control" id="beds_number" name="beds_number" value="{{ old('beds_number') ? old('beds_number') : $apartment->beds_number }}" required min="1" max="255">
     </div>
     {{-- /BEDS NUMBER --}}
 
     {{-- MQS --}}
     <div class="mb-3">
-      <label for="mqs" class="form-label">Metri quadrati</label>
+      <label for="mqs" class="form-label">Metri quadrati *</label>
       <input type="number" class="form-control" id="mqs" name="mqs" value="{{ old('mqs') ? old('mqs') : $apartment->mqs }}" required min="1" max="65535">
     </div>
     {{-- /MQS --}}
 
     {{-- ADDRESS --}}
     <div class="mb-3">
-      <label for="address" class="form-label">Indirizzo</label>
+      <label for="address" class="form-label">Indirizzo *</label>
       <input type="text" class="form-control" id="address" name="address" value="{{ old('address') ? old('address') : $apartment->address }}" required minlength="4" maxlength="255">
     </div>
     {{-- /ADDRESS --}}
 
     {{-- COVER IMAGE --}}
     <div class="mb-3">
-      <label for="image-cover">Aggiungi un immagine</label>
+      <label for="image-cover">Aggiungi un immagine *</label>
       <input type="file" id="image-cover" name="image-cover" accept=".jpg, .jpeg, .png, .bmp, .gif, .svg, .webp">
     </div>
     {{-- /COVER IMAGE --}}
 
     {{-- EXTRA SERVICES --}}
-    <p>Servizi Extra</p>
+    <p>Servizi Extra *</p>
     @foreach ($extra_services as $extra_service)
     <div class="form-check">
       <input class="form-check-input" type="checkbox" value="{{ $extra_service->id }}" id="extra_service-{{ $extra_service->id }}" name="extra_services[]" {{ ($apartment->services->contains($extra_service) || in_array($extra_service->id, old('extra_services', []))) ? 'checked' : '' }}>
@@ -96,9 +96,9 @@
 
     {{-- VISIBILITY --}}
     <div class="form-check">
-      <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault">
-      <label class="form-check-label" for="flexCheckDefault">
-        Vuoi rendere questo appartemento visibile?
+      <input class="form-check-input" type="checkbox" value="" id="flexCheckChecked" checked>
+      <label class="form-check-label" for="flexCheckChecked">
+        Vuoi rendere questo appartemento visibile? *
       </label>
     </div>
     {{-- /VISIBILITY --}}
