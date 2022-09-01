b7ce96b0d35678e5138fba328ffa7082ce58b7c9
SydVic <fabrizio.bocca27@gmail.com>
2022-09-01 15:55:20 +0200

    hover e pointer ai tips address

--- public/js/app.js
+++ public/js/app.js
@@ -132,15 +132,27 @@
 
 /***/ }),
 
+/***/ "./resources/sass/form.scss":
+/*!**********************************!*\
+  !*** ./resources/sass/form.scss ***!
+  \**********************************/
+/*! no static exports found */
+/***/ (function(module, exports) {
+
+throw new Error("Module build failed (from ./node_modules/css-loader/index.js):\nModuleBuildError: Module build failed (from ./node_modules/sass-loader/dist/cjs.js):\nError: ENOENT: no such file or directory, open 'C:\\boolean\\progetto-finale\\laravel-bool_bb\\resources\\sass\\form.scss'\n    at C:\\boolean\\progetto-finale\\laravel-bool_bb\\node_modules\\webpack\\lib\\NormalModule.js:316:20\n    at C:\\boolean\\progetto-finale\\laravel-bool_bb\\node_modules\\loader-runner\\lib\\LoaderRunner.js:367:11\n    at C:\\boolean\\progetto-finale\\laravel-bool_bb\\node_modules\\loader-runner\\lib\\LoaderRunner.js:203:19\n    at C:\\boolean\\progetto-finale\\laravel-bool_bb\\node_modules\\enhanced-resolve\\lib\\CachedInputFileSystem.js:85:15\n    at processTicksAndRejections (node:internal/process/task_queues:78:11)");
+
+/***/ }),
+
 /***/ 0:
-/*!*************************************************************!*\
-  !*** multi ./resources/js/app.js ./resources/sass/app.scss ***!
-  \*************************************************************/
+/*!****************************************************************************************!*\
+  !*** multi ./resources/js/app.js ./resources/sass/app.scss ./resources/sass/form.scss ***!
+  \****************************************************************************************/
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
 __webpack_require__(/*! C:\boolean\progetto-finale\laravel-bool_bb\resources\js\app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! C:\boolean\progetto-finale\laravel-bool_bb\resources\sass\app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! C:\boolean\progetto-finale\laravel-bool_bb\resources\sass\app.scss */"./resources/sass/app.scss");
+module.exports = __webpack_require__(/*! C:\boolean\progetto-finale\laravel-bool_bb\resources\sass\form.scss */"./resources/sass/form.scss");
 
 
 /***/ })
